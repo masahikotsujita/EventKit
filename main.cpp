2562adb66522fa6070de0faea59dd081b601f430
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2019-08-13 00:59:21 +0900

    Update example

--- main.cpp
+++ main.cpp
@@ -2,8 +2,42 @@
 // Created by Masahiko Tsujita on 2019-08-05.
 //
 
-#include <cstdio>
+#include <iostream>
+#include <thread>
+#include "RunLoop.h"
+#include "DispatchQueue.h"
 
 int main(int argc, const char* argv[]) {
-    printf("Hello, world!\n");
+    ek::RunLoop mainLoop;
+    auto pMainQueue = std::make_shared<ek::DispatchQueue>();
+    mainLoop.addDispatchQueue(pMainQueue);
+
+    ek::RunLoop bgLoop;
+    auto pBgQueue = std::make_shared<ek::DispatchQueue>();
+    bgLoop.addDispatchQueue(pBgQueue);
+
+    pMainQueue->dispatchAsync([] {
+        printf("a task running on main thread.\n");
+    });
+
+    pBgQueue->dispatchAsync([] {
+        printf("a task running on background thread.\n");
+    });
+
+    pBgQueue->dispatchAsync([&] {
+        printf("dispatching a task to main thread from background thread.\n");
+        pMainQueue->dispatchAsync([] {
+            printf("a task running on main thread dispatched from background thread.\n");
+        });
+    });
+
+    std::thread bgThread([&]{
+        bgLoop.run();
+    });
+
+    mainLoop.run();
+
+    bgThread.join();
+
+    return 0;
 }
