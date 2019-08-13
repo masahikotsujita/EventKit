dc119c157c293cbb6716617d1175578b98e108d6
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2019-08-13 09:35:37 +0900

    Split Semaphore members into declaration and definition

--- include/detail/Semaphore.h
+++ include/detail/Semaphore.h
@@ -13,39 +13,16 @@ namespace ek::detail {
 class Semaphore final {
 public:
 
-    explicit Semaphore(int maxCount = COUNT_MAX_DEFAULT)
-        : m_maxCount(maxCount)
-        , m_count(0)
-        , m_mutex()
-        , m_conditionVariable() {
-    }
+    explicit Semaphore(int maxCount = COUNT_MAX_DEFAULT);
 
     ~Semaphore() = default;
 
-    void wait() {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        m_conditionVariable.wait(lock, [&]{ return m_count > 0; });
-        m_count -= 1;
-    }
+    void wait();
 
     template <typename Clock, typename Duration>
-    bool waitUntil(const std::chrono::time_point<Clock, Duration>& timeoutTime) {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        if (!m_conditionVariable.wait_until(lock, timeoutTime, [&]{ return m_count > 0; })) {
-            return false;
-        }
-        m_count -= 1;
-        return true;
-    }
+    bool waitUntil(const std::chrono::time_point<Clock, Duration>& timeoutTime);
 
-    void notify() {
-        std::unique_lock<std::mutex> lock(m_mutex);
-        if (m_count < m_maxCount) {
-            m_count += 1;
-            lock.unlock();
-            m_conditionVariable.notify_one();
-        }
-    }
+    void notify();
 
 public:
     Semaphore(const Semaphore&) = delete;
@@ -59,6 +36,38 @@ private:
     std::condition_variable m_conditionVariable;
 
 };
+
+inline Semaphore::Semaphore(int maxCount)
+    : m_maxCount(maxCount)
+    , m_count(0)
+    , m_mutex()
+    , m_conditionVariable() {
+}
+
+inline void Semaphore::wait() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    m_conditionVariable.wait(lock, [&]{ return m_count > 0; });
+    m_count -= 1;
+}
+
+template <typename Clock, typename Duration>
+bool Semaphore::waitUntil(const std::chrono::time_point<Clock, Duration>& timeoutTime) {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    if (!m_conditionVariable.wait_until(lock, timeoutTime, [&]{ return m_count > 0; })) {
+        return false;
+    }
+    m_count -= 1;
+    return true;
+}
+
+inline void Semaphore::notify() {
+    std::unique_lock<std::mutex> lock(m_mutex);
+    if (m_count < m_maxCount) {
+        m_count += 1;
+        lock.unlock();
+        m_conditionVariable.notify_one();
+    }
+}
 
 }
 
