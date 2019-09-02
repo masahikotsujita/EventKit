047c72943959d81a35f90555b2188796eabd5897
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2019-09-03 00:52:57 +0900

    Move promise operator classes into detail/

--- include/eventkit/promise/operators/done.h
+++ include/eventkit/promise/operators/done.h
@@ -5,36 +5,12 @@
 #ifndef EVENTKIT_DONE_H
 #define EVENTKIT_DONE_H
 
+#include <eventkit/promise/operators/detail/done-inl.h>
 #include <type_traits>
 
 namespace ek {
 namespace promise {
 namespace operators {
-
-namespace detail {
-
-template <typename Handler>
-class DoneOperator {
-public:
-    template <typename F>
-    explicit DoneOperator(F&& handler)
-        : m_handler(std::forward<F>(handler)) {
-    }
-    
-    template <typename PromiseType>
-    decltype(auto) operator()(PromiseType&& promise) && {
-        using T = typename PromiseType::Value;
-        using E = typename PromiseType::Error;
-        promise.pipe(ek::promise::detail::make_function_observer<T, E>(std::forward<Handler>(m_handler)));
-        return std::forward<PromiseType>(promise);
-    }
-
-private:
-    Handler m_handler;
-    
-};
-    
-}
 
 template <typename Handler>
 auto done(Handler&& handler) -> detail::DoneOperator<Handler&&> {
--- include/eventkit/promise/operators/then.h
+++ include/eventkit/promise/operators/then.h
@@ -5,40 +5,12 @@
 #ifndef EVENTKIT_THEN_H
 #define EVENTKIT_THEN_H
 
+#include <eventkit/promise/operators/detail/then-inl.h>
 #include <type_traits>
-#include <eventkit/promise/detail/ThenTransformationCore.h>
-#include <eventkit/promise/Promise.h>
 
 namespace ek {
 namespace promise {
 namespace operators {
-
-namespace detail {
-
-template <typename Handler>
-class ThenOperator {
-public:
-    template <typename F>
-    explicit ThenOperator(F&& handler)
-        : m_handler(std::forward<F>(handler)) {
-    }
-    
-    template <typename PromiseType>
-    auto operator()(PromiseType&& promise) && {
-        using T = typename PromiseType::Value;
-        using E = typename PromiseType::Error;
-        using U = typename std::result_of_t<Handler(T)>::Value;
-        auto pCore = std::make_shared<ek::promise::detail::ThenTransformationCore<T, E, U, std::decay_t<Handler>>>(std::forward<std::decay_t<Handler>>(m_handler));
-        promise.pipe(pCore->asHandler());
-        return Promise<U, E>(pCore->asCore());
-    }
-
-private:
-    Handler m_handler;
-
-};
-
-}
 
 template <typename Handler>
 auto then(Handler&& handler) -> detail::ThenOperator<Handler&&> {
