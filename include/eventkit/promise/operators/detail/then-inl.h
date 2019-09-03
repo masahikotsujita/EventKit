2cfeda79d3a688327ad5bafbf98e95adade4dade
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2019-09-04 01:03:32 +0900

    Add private Promise construction function to unify friend declarations

--- include/eventkit/promise/operators/detail/then-inl.h
+++ include/eventkit/promise/operators/detail/then-inl.h
@@ -28,7 +28,7 @@ public:
         using U = typename std::result_of_t<Handler(T)>::Value;
         auto pCore = std::make_shared<ek::promise::detail::ThenTransformationCore<T, E, U, std::decay_t<Handler>>>(std::forward<std::decay_t<Handler>>(m_handler));
         promise.pipe(pCore->asHandler());
-        return Promise<U, E>(pCore->asCore());
+        return ek::promise::detail::make_promise(pCore->asCore());
     }
 
 private:
