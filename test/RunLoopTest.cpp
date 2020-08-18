775ce2760720f5afd1f45ab8ccff5b1f4bd9297a
Masahiko Tsujita <masahiko.tsujita@icloud.com>
2020-08-18 09:05:16 +0900

    Update async test assertions.

--- test/RunLoopTest.cpp
+++ test/RunLoopTest.cpp
@@ -16,12 +16,15 @@ SCENARIO("a run loop", "[run_loop]") {
         auto pRunLoop = std::make_shared<ek::dispatch::RunLoop>();
 
         WHEN("the run loop starts running on a thread") {
-            auto pThread = std::make_shared<std::thread>([&]{
+            DECLARE_FLAG(RUNLOOP_END);
+            std::thread thread([=]{
                 pRunLoop->run();
+                SET_FLAG(RUNLOOP_END);
             });
+            thread.detach();
 
             THEN("the thread will terminate immediately") {
-                REQUIRE_RETURN_WITH_TIMEOUT(pThread->join(), 5s);
+                REQUIRE_FLAG_SET(RUNLOOP_END, 5s);
             }
         }
 
@@ -29,21 +32,16 @@ SCENARIO("a run loop", "[run_loop]") {
             auto pDispatchQueue = ek::common::make_intrusive<ek::dispatch::DispatchQueue>();
             pRunLoop->addDispatchQueue(pDispatchQueue);
 
-            auto pTriplet = std::make_shared<std::tuple<std::mutex, std::condition_variable, std::atomic_bool>>();
-            std::get<2>(*pTriplet) = false;
-
             WHEN("the run loop starts running on a thread") {
-                std::thread thread([pTriplet, pRunLoop]{
+                DECLARE_FLAG(RUNLOOP_END);
+                std::thread thread([=]{
                     pRunLoop->run();
-                    std::get<2>(*pTriplet) = true;
-                    std::get<1>(*pTriplet).notify_all();
+                    SET_FLAG(RUNLOOP_END);
                 });
                 thread.detach();
 
                 THEN("the thread will keep running") {
-                    std::unique_lock<std::mutex> lock(std::get<0>(*pTriplet));
-                    auto timeout = std::chrono::system_clock::now() + 5s;
-                    REQUIRE(!std::get<1>(*pTriplet).wait_until(lock, timeout, [&]{ return std::get<2>(*pTriplet).load(); }));
+                    REQUIRE_NOT_FLAG_SET(RUNLOOP_END, 5s);
                 }
 
 //                WHEN("the event source is removed from the run loop") {
